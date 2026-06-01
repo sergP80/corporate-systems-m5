81125b22d7d341686dff80b8417e297375cf39ca
Sergii <sergii5591@gmail.com>
2026-06-01 14:47:46 +0300

    Added documentation for api

--- src/main/java/ua/edu/chmnu/ki/m5/cs/controller/HelloApi.java
+++ src/main/java/ua/edu/chmnu/ki/m5/cs/controller/HelloApi.java
@@ -1,5 +1,10 @@
 package ua.edu.chmnu.ki.m5.cs.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Tag(name = "Hello")
 public interface HelloApi {
 
     String ping(String payload);
